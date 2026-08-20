4f5b6f9a12278f14f25ab72ca9233053df3beecb
walshl2662 <walshl26@tcd.ie>
2026-08-20 20:55:56 +0100

    Track remaining players after game start

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
+
 players = []
+poem = []
+players_remaining = []
+game_started = False
+current_player = None
+
 
 @app.route("/")
 def home():
-    return render_template("index.html", players=players)
+    return render_template(
+        "index.html",
+        players=players,
+        game_started=game_started,
+        current_player=current_player
+    )
+
 
 @app.route("/join", methods=["POST"])
 def join():
@@ -14,7 +27,34 @@ def join():
 
     players.append(player_name)
 
-    return render_template("index.html", players=players)
+    return render_template(
+        "index.html",
+        players=players,
+        game_started=game_started
+    )
+
+
+@app.route("/start", methods=["POST"])
+def start():
+
+    global game_started, current_player, players_remaining
+
+    game_started = True
+    players_remaining=players.copy()
+    current_player=random.choice(players_remaining)
+    players_remaining.remove(current_player) 
+
+    print(players)
+    print(players_remaining)
+    print(current_player)
+
+    return render_template(
+        "index.html",
+        players=players,
+        game_started=game_started,
+        current_player=current_player
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
