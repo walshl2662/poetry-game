44e11d848117c13cbeef21c78775d5af9aa63744
walshl2662 <walshl26@tcd.ie>
2026-08-20 19:58:34 +0100

    Players can now register their nicknames

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+players = []
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", players=players)
+
+@app.route("/join", methods=["POST"])
+def join():
+
+    player_name = request.form["playerName"]
+
+    players.append(player_name)
+
+    return render_template("index.html", players=players)
 
 if __name__ == "__main__":
     app.run(debug=True)
